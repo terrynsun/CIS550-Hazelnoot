c49132334930552a198019ec1b6d3641b80fc2d7
Daniel Ge <dange@seas.upenn.edu>
2013-12-06 15:36:53 -0500

    Add model validations for User

--- models/user.js
+++ models/user.js
@@ -4,24 +4,47 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define("User", {
         first_name: {
             type: DataTypes.STRING(32),
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
         },
         last_name: {
             type: DataTypes.STRING(32),
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
         },
         user_name: {
             type: DataTypes.STRING(32),
             primaryKey: true,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
         },
         email: {
             type: DataTypes.STRING(64),
             unique: true,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isEmail: true,
+                max: 64
+            }
         },
         affiliation: DataTypes.STRING(64),
-        password_hash: DataTypes.STRING(60),
+        password_hash: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+            validate: {
+                min: 60,
+                max: 60
+            }
+        },
         birthday: DataTypes.DATE
     }, {
         tableName: 'Users'

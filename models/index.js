f74772ff63e7408dbef0dd8089aa7bfefc0b7dbd
Terry Sun <terrynsun@gmail.com>
2013-12-07 02:41:49 -0500

    Added pin and tag models

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ var Interest = sequelize.import(__dirname + '/interest');
 var Friendship = sequelize.import(__dirname + '/friendship');
 var Board = sequelize.import(__dirname + '/board');
 var Rating = sequelize.import(__dirname + '/rating');
+var Pin = sequelize.import(__dirname + '/pin');
+var Tags = sequelize.import(__dirname + '/tags');
 
 User.hasMany(Interest, {
     as: 'Interests',
@@ -23,14 +25,35 @@ User.hasMany(Rating, {
     as: 'Ratings',
     foreignKey: 'user_name'
 }),
+User.hasMany(Pin, {
+    as: 'Pin',
+    foreignKey: 'user_name'
+}),
+
 PinObject.hasMany(Rating, {
     as: 'Ratings',
     foreignKey: 'object_id'
 });
+PinObject.hasMany(Pin, {
+    as: 'Pin',
+    foreignKey: 'object_id'
+});
+PinObject.hasMany(Tags, {
+    as: 'Tags',
+    foreignKey: 'object_id'
+});
+
+/* Let's pretend this is actual code n' stuff.
+Board.hasMany(Pin, {
+    as: 'Pin',
+    foreignKey: 'user_name',
+    foreignKey: 'board_name'
+});
+*/
 
 
 exports.User = User;
-exports.PinObject = Object;
+exports.PinObject = PinObject;
 exports.Interest = Interest;
 exports.Board = Board;
 exports.Rating = Rating;
---
+++ models/pin.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('Pin', {
+        user_name: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
+        },
+        object_id: DataTypes.INTEGER(11),
+        board_name: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 64
+            }
+        },
+        time_created: DataTypes.DATE
+    }, {
+        tableName: "Pin",
+        timestamps: false
+    });
+};

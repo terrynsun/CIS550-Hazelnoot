e25c95edd1f24c544b03406c56c45265b7950529
Daniel Ge <dange@seas.upenn.edu>
2013-12-06 23:38:10 -0500

    Expose Object model through models/index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var sequelize = require('../app_config/sequelize');
 
 var User = sequelize.import(__dirname + '/user');
+var Object = sequelize.import(__dirname + '/object');
 
 exports.User = User;
+exports.Object = Object;

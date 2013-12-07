5b2a59c4e1c6f200c298bc75f395f0290ff39d02
Daniel Ge <dange@seas.upenn.edu>
2013-12-07 01:10:17 -0500

    Add Friendship model

---
+++ models/friendship.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define("Friendship", {
+        user_name: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
+        },
+        friend_name: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                max: 32
+            }
+        },
+        time_created: DataTypes.DATE
+    }, {
+        tableName: "Friendship",
+        timestamps: false,
+
+        instanceMethods: {
+            name: function() {
+                return this.friend_name;
+            }
+        }
+    });
+};
--- models/index.js
+++ models/index.js
@@ -3,9 +3,14 @@ var sequelize = require('../app_config/sequelize');
 var User = sequelize.import(__dirname + '/user');
 var Object = sequelize.import(__dirname + '/object');
 var Interest = sequelize.import(__dirname + '/interest');
+var Friendship = sequelize.import(__dirname + '/friendship');
 
 User.hasMany(Interest, {
     as: 'Interests',
+    foreignKey: 'user_name'
+});
+User.hasMany(Friendship, {
+    as: 'Friends',
     foreignKey: 'user_name'
 });
 

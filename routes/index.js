99aa8efcd81344dfd664c481e9c990601b4748d5
Terry Sun <terrynsun@gmail.com>
2013-12-07 02:52:25 -0500

    A few completely pointless cosmetic changes.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', current_user: req.user });
+  res.render('index', { title: 'Hazlenoot', current_user: req.user });
 };

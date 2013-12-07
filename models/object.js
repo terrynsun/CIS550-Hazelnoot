583089f25d7a6239bb67cdc019828dc8d3068e5c
Daniel Ge <dange@seas.upenn.edu>
2013-12-06 19:41:17 -0500

    Tabs -> spaces in Object model >_>

--- models/object.js
+++ models/object.js
@@ -1,31 +1,31 @@
 var Sequelize = require('sequelize');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define("Object", {
-		id: {
-			type: DataTypes.INTEGER(11),
-			primaryKey: true,
-			allowNull: false
-		},
-		other_id: DataTypes.INTEGER(11),
-		source: DataTypes.STRING(32),
-		type: {
-			type: STRING(16),
-			allowNull : false,
-			validate:{
-				notEmpty: true,
-				max: 16
-			}
-		}
-		url: {
-			type: DataTypes.STRING(256),
-			allowNull: false,
-			validate: {
-				isUrl: true,
-				max: 256
-			}
-		} 
-	}, {
-		tableName: "Object"
-	})
+    return sequelize.define("Object", {
+        id: {
+            type: DataTypes.INTEGER(11),
+            primaryKey: true,
+            allowNull: false
+        },
+        other_id: DataTypes.INTEGER(11),
+        source: DataTypes.STRING(32),
+        type: {
+            type: STRING(16),
+            allowNull : false,
+            validate:{
+                notEmpty: true,
+                max: 16
+            }
+        },
+        url: {
+            type: DataTypes.STRING(256),
+            allowNull: false,
+            validate: {
+                isUrl: true,
+                max: 256
+            }
+        }
+    }, {
+        tableName: "Object"
+    })
 };

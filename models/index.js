b4ab24af0cd19ab95591edbeea4ff66dcc5b5dc2
rroberts1027 <rrobertsdev@gmail.com>
2013-12-07 01:50:22 -0500

    Changed Board.js to board.js and updated index.js

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ var User = sequelize.import(__dirname + '/user');
 var Object = sequelize.import(__dirname + '/object');
 var Interest = sequelize.import(__dirname + '/interest');
 var Friendship = sequelize.import(__dirname + '/friendship');
+var Board = sequelize.import(__dirname + '/board');
 
 User.hasMany(Interest, {
     as: 'Interests',
@@ -13,7 +14,12 @@ User.hasMany(Friendship, {
     as: 'Friends',
     foreignKey: 'user_name'
 });
+User.hasMany(Board, {
+    as: 'Boards',
+    foreignKey: 'owner_name'
+});
 
 exports.User = User;
 exports.Object = Object;
 exports.Interest = Interest;
+exports.Board = Board;

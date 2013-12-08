333aff48c2f6e278693d9e34b6ed9233c1bb5a2e
Daniel Ge <dange@seas.upenn.edu>
2013-12-08 01:44:56 -0500

    Make a column of Pin a primary key
    Sequelize is opinionated and thinks that if you don't specify a primary
    key, then it needs to interfere and add one itself (an 'id' column).
    This problem comes up during INSERTs.

--- models/pin.js
+++ models/pin.js
@@ -6,7 +6,12 @@ module.exports = function(sequelize, DataTypes) {
             validate: {
                 notEmpty: true,
                 max: 32
-            }
+            },
+
+            // IGNORE THIS.
+            // Sequelize is kinda opinionated. If you don't define a primary key, it
+            // will just assume that you want an ID column >_>
+            primaryKey: true
         },
         object_id: DataTypes.INTEGER(11),
         board_name: {

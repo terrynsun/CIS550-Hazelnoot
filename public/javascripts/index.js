f8acf8d51e2d4e59f169ac24c9636d047a165898
Daniel Ge <dange@seas.upenn.edu>
2013-12-07 22:42:38 -0500

    Add UI for adding pins

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var $newPinForm = $('#new-pin-form');
+    var $newPin = $('#new-pin');
+    var $urlInput = $('#url-input');
+    $newPin.click(function(e) {
+        $newPinForm.removeClass('hidden').addClass('show');
+        $(this).addClass('hidden');
+        $urlInput.focus();
+        e.preventDefault();
+    });
+
+    $newPinForm.focusout(function() {
+        $newPin.removeClass('hidden');
+        $(this).addClass('hidden');
+    })
+
+});

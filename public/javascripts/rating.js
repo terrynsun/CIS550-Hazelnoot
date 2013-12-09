61dfcd39b7376352b8b3a0c2f2363e93828c9661
Daniel Ge <dange@seas.upenn.edu>
2013-12-09 02:29:19 -0500

    Here's your jQuery skeleton code

---
+++ public/javascripts/rating.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#ratings button').each(function(){
+        var $this = $(this);
+        var rating = $this.data('rating');
+
+        var success = function(data, textStatus) {
+            // TODO: Fill this in with stuff
+            console.log('Success!');
+        };
+        $this.click(function(e) {
+            console.log('clicked ' + rating);
+            // Asynchronous POST request
+            // See http://api.jquery.com/jQuery.post/
+            $.post('URL HERE', { rating: rating }, success);
+            e.preventDefault();
+        });
+    })
+});

811321a6923516c2f0d07136a8c4b4338dc5e72b
Daniel Ge <dange@seas.upenn.edu>
2013-12-16 20:46:14 -0500

    Update ratings to be truly async

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
+    var $ratings = $('#ratings');
+    var id = $ratings.data('id');
+    var last_rating = $ratings.data('current-rating');
+
     $('#ratings button').each(function(){
         var $this = $(this);
+        $this.button();
         var rating = $this.data('rating');
 
+        if (rating === last_rating) {
+            $this.button('toggle');
+        }
+
         var success = function(data, textStatus) {
-            // TODO: Fill this in with stuff
-            console.log('Success!');
+            if (!data.success) {
+                console.log('Opposite of success');
+            }
         };
         $this.click(function(e) {
             console.log('clicked ' + rating);
             // Asynchronous POST request
             // See http://api.jquery.com/jQuery.post/
-            $.post('/rating/:id', { rating: rating }, success);
+            $.post('/rating/' + id, { rating: rating }, success);
             e.preventDefault();
         });
     })

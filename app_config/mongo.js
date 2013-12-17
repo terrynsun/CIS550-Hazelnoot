610c8e593ab770294c4dee9938cde43d81a2875e
Daniel Ge <dange@seas.upenn.edu>
2013-12-17 05:42:44 -0500

    Add sensible Mongo defaults

--- app_config/mongo.js
+++ app_config/mongo.js
@@ -1,7 +1,18 @@
 var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
 var Q = require('q');
 require('js-yaml');
 var config = require('../config/db.yaml').mongo;
 
-exports.read_connect = Q.nfcall(MongoClient.connect, config.readonly);
-exports.write_connect = Q.nfcall(MongoClient.connect, config.pennterest);
+var serverConfig = {
+    native_parser: true,
+    auto_reconnect: true,
+    socketOptions: {
+        keepAlive: 300,
+        connectTimeoutMS: 20,
+        socketTimeoutMS: 10
+    }
+};
+
+exports.read_connect = Q.nfcall(MongoClient.connect, config.readonly, serverConfig);
+exports.write_connect = Q.nfcall(MongoClient.connect, config.pennterest, serverConfig);
